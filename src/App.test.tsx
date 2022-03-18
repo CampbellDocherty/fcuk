3380d7ed443a17391ec770bdc7dc1bc1d05e7f44
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2022-03-18 16:28:01 +0000

    Add title and random asset and test

---
+++ src/App.test.tsx
@@ -0,0 +1,21 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('When a user lands on the home page', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+  afterEach(cleanup);
+  it.each([
+    ['first left arrow'],
+    ['second left arrow'],
+    ['third left arrow'],
+    ['first right arrow'],
+    ['second right arrow'],
+    ['third right arrow'],
+    ['fcuk title'],
+    ['random button'],
+  ])('shows the %s', (altText: string) => {
+    screen.getByAltText(altText);
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React, { FC } from 'react';
 import LeftArrow from './assets/fcuk-arrow-left.png';
 import RightArrow from './assets/fcuk-arrow-right.png';
+import RandomButton from './assets/fcuk-random-button.png';
+import Title from './assets/fcuk-title.png';
 
 const App: FC = () => {
   return (
     <>
+      <img src={Title} alt="fcuk title" />;
       <img src={LeftArrow} alt="first left arrow" />;
       <img src={LeftArrow} alt="second left arrow" />;
       <img src={LeftArrow} alt="third left arrow" />;
       <img src={RightArrow} alt="first right arrow" />;
       <img src={RightArrow} alt="second right arrow" />;
       <img src={RightArrow} alt="third right arrow" />;
+      <img src={RandomButton} alt="random button" />;
     </>
   );
 };
