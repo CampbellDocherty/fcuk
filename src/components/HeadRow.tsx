8347d54ab9b021f9c17041a5235d97a4ffc297bf
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2022-03-19 17:21:50 +0000

    Loop back to beginning on next at last head image

--- src/components/HeadRow.tsx
+++ src/components/HeadRow.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import LeftArrow from '../assets/fcuk-arrow-left.png';
 import RightArrow from '../assets/fcuk-arrow-right.png';
+import { headImages } from '../assets/images';
+import { HeadImage } from '../styles';
 import Arrow from './Arrow';
-import Head from './Head';
 
 const HeadRow = () => {
   const [headCount, setHeadCount] = useState<number>(1);
   const onNextHeadClick = () => {
+    if (headCount === headImages.length) {
+      return setHeadCount(1);
+    }
     setHeadCount(headCount + 1);
   };
   const onPreviousHeadClick = () => {
@@ -21,7 +25,16 @@ const HeadRow = () => {
         column={2}
         onClick={onPreviousHeadClick}
       />
-      <Head headCount={headCount} />
+      {headImages.map((src, index) => {
+        return (
+          <HeadImage
+            key={index}
+            src={src}
+            alt={`head image ${index + 1}`}
+            shouldDisplay={index + 1 === headCount}
+          />
+        );
+      })}
       <Arrow
         src={RightArrow}
         altText={'first right arrow'}
