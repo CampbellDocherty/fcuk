deb7ef080e8ec85b2e0dc7c928a4d37f0b18dbdf
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2022-03-23 13:45:11 +0000

    Fix lint errors

--- src/specs/App.test.tsx
+++ src/specs/App.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/dom';
+import { screen } from '@testing-library/react';
 import { cleanup, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
