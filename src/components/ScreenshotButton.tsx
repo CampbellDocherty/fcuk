ee2e59edb7d6dd6a309f026fe0ecc4ad930a09cb
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2022-03-29 17:25:10 +0100

    On screenshot also upload image to firebase

--- src/components/ScreenshotButton.tsx
+++ src/components/ScreenshotButton.tsx
@@ -1,12 +1,23 @@
 /* eslint-disable functional/immutable-data */
+import { getStorage, ref as storageRef, uploadString } from 'firebase/storage';
 import React from 'react';
 import { useScreenshot, createFileName } from 'use-react-screenshot';
+import firebaseApp from '../firebase';
 
 const ScreenshotButton = React.forwardRef((props, ref: any) => {
+  const firebaseStorage = getStorage(firebaseApp);
   const [, takeScreenShot] = useScreenshot({
     type: 'image/jpeg',
     quality: 1.0,
   });
+
+  const upload = async (screenshot: any) => {
+    const base64Image = screenshot.replace(/data:image\/jpeg;base64,/g, '');
+    const firebaseRef = storageRef(firebaseStorage, new Date().toString());
+    await uploadString(firebaseRef, base64Image, 'base64', {
+      contentType: 'image/jpeg',
+    });
+  };
 
   const download = (
     screenshot: any,
@@ -16,9 +27,12 @@ const ScreenshotButton = React.forwardRef((props, ref: any) => {
     a.href = screenshot;
     a.download = createFileName(extension, name);
     a.click();
+    return screenshot;
   };
 
-  const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
+  const downloadScreenshot = () =>
+    takeScreenShot(ref.current).then(download).then(upload);
+
   return <button onClick={downloadScreenshot}>Screenshot</button>;
 });
 
