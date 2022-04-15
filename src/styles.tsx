abc961e5b90834dd044c25e52e7cf9c8520d0a5a
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2022-04-15 12:36:37 +0100

    Add screenshot and random button images

--- src/styles.tsx
+++ src/styles.tsx
@@ -60,14 +60,28 @@ export const RandomButtonImage = styled.img<{ readonly mouseDown: boolean }>`
   cursor: pointer;
 `;
 
+export const ScreenshotButtonImage = styled.img<{
+  readonly mouseDown: boolean;
+}>`
+  width: ${(props) => (props.mouseDown ? '97%' : '100%')};
+  cursor: pointer;
+`;
+
 export const StyledButton = styled.button`
-  width: 30%;
+  max-width: 60px;
   background-color: white;
   grid-column: 3;
   grid-row: 5;
   border: none;
   border-radius: 300px;
   padding: 0;
+
+  :nth-of-type(1) {
+    margin-right: 16px;
+  }
+  :nth-of-type(2) {
+    margin-left: 16px;
+  }
 `;
 
 export const HeadImage = styled.img<{ readonly shouldDisplay: boolean }>`
